f5368fff6de59f4b649f917eb9739c39e62b1428
Stuart R. Jefferys <srjefferys@gmail.com>
2013-08-04 14:47:59 -0400

    Initial template changes in place.

--- lib/Bio/SeqWare/Uploads/CgHub/Fastq.pm
+++ lib/Bio/SeqWare/Uploads/CgHub/Fastq.pm
@@ -1,12 +1,15 @@
 package Bio::SeqWare::Uploads::CgHub::Fastq;
 
-use 5.008;
-use strict;
-use warnings FATAL => 'all';
+use 5.008;         # No reason, just being specific. Update your perl.
+use strict;        # Don't allow unsafe perl constructs.
+use warnings;      # Enable all optional warnings.
+use Carp;          # Base the locations of reported errors on caller's code.
 
 =head1 NAME
 
 Bio::SeqWare::Uploads::CgHub::Fastq - Support uploads of fastq files to cghub
+
+=cut
 
 =head1 VERSION
 
@@ -16,103 +19,138 @@ Version 0.000.001   # PRE-RELEASE
 
 our $VERSION = '0.000001';   # PRE-RELEASE
 
-
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+
 
 Perhaps a little code snippet.
 
     use Bio::SeqWare::Uploads::CgHub::Fastq;
 
-    my $foo = Bio::SeqWare::Uploads::CgHub::Fastq->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
+    my $obj = Bio::SeqWare::Uploads::CgHub::Fastq->new();
 
 =cut
 
-sub function1 {
-}
+=head1 DESCRIPTION
 
-=head2 function2
+Supports the upload of zipped fastq file sets for samples to cghub. Includes
+db interactions, zip command line convienience functions, and meta-data
+generation control. The meta-data uploads are a hack on top of a current
+implementation, just generates the current version, then after-the-fact
+modifies it to do a fastq upload.
 
 =cut
 
-sub function2 {
+=head1 CLASS METHODS
+
+=cut
+
+=head2 new()
+
+    my $obj = Bio::SeqWare::Uploads::CgHub::Fastq->new();
+
+Creates and returns a Bio::SeqWare::Uploads::CgHub::Fastq object. Takes
+no parameters.
+
+=cut
+
+sub new {
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    return $self;
 }
+
+=head1 INSTANCE METHODS
+
+    NONE
+
+=cut
+
+=head1 INTERNAL METHODS
+
+NOTE: These methods are for I<internal use only>. They are documented here
+mainly due to the effort needed to separate user and developer documentation.
+Pay no attention to code behind the curtain; these are not the methods you are
+looking for. If you use these function I<you are doing something wrong.>
+
+    NONE
+
+=cut
 
 =head1 AUTHOR
 
 Stuart R. Jefferys, C<< <srjefferys (at) gmail (dot) com> >>
 
-=head1 BUGS
+=cut
 
-Please report any bugs or feature requests to C<bug-p5-bio-seqware-uploads-cghub-fastq at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=p5-Bio-SeqWare-Uploads-CgHub-Fastq>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+=head1 DEVELOPMENT
 
+This module is developed and hosted on GitHub, at
+L<p5-Bio-SeqWare-Config https://github.com/theobio/p5-Bio-SeqWare-Uploads-CgHub-Fastq>.
+It is not currently on CPAN, and I don't have any immediate plans to post it
+there unless requested by core SeqWare developers (It is not my place to
+set out a module name hierarchy for the project as a whole :)
 
+=cut
 
+=head1 INSTALLATION
 
-=head1 SUPPORT
+You can install a version of this module directly from github using
 
-You can find documentation for this module with the perldoc command.
+    $ cpanm git://github.com/theobio/p5-Bio-SeqWare-Uploads-CgHub-Fastq.git@v0.000.001
 
-    perldoc Bio::SeqWare::Uploads::CgHub::Fastq
+Any version can be specified by modifying the tag name, following the @;
+the above installs the latest I<released> version. If you leave off the @version
+part of the link, you can install the bleading edge pre-release, if you don't
+care about bugs...
 
+You can select and download any package for any released version of this module
+directly from L<https://github.com/theobio/p5-Bio-SeqWare-Uploads-CgHub-Fastq/releases>.
+Installing is then a matter of unzipping it, changing into the unzipped
+directory, and then executing the normal (C>Module::Build>) incantation:
 
-You can also look for information at:
+     perl Build.PL
+     ./Build
+     ./Build test
+     ./Build install
 
-=over 4
+=cut
 
-=item * RT: CPAN's request tracker (report bugs here)
+=head1 BUGS AND SUPPORT
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=p5-Bio-SeqWare-Uploads-CgHub-Fastq>
+No known bugs are present in this release. Unknown bugs are a virtual
+certainty. Please report bugs (and feature requests) though the
+Github issue tracker associated with the development repository, at:
 
-=item * AnnoCPAN: Annotated CPAN documentation
+L<https://github.com/theobio/p5-Bio-SeqWare-Uploads-CgHub-Fastq/issues>
 
-L<http://annocpan.org/dist/p5-Bio-SeqWare-Uploads-CgHub-Fastq>
+Note: you must have a GitHub account to submit issues.
 
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/p5-Bio-SeqWare-Uploads-CgHub-Fastq>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/p5-Bio-SeqWare-Uploads-CgHub-Fastq/>
-
-=back
-
+=cut
 
 =head1 ACKNOWLEDGEMENTS
 
+This module was developed for use with L<SegWare | http://seqware.github.io>.
+
+=cut
 
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2013 Stuart R. Jefferys.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; version 2 dated June, 1991 or at your option
-any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-A copy of the GNU General Public License is available in the source tree;
-if not, write to the Free Software Foundation, Inc.,
-59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
 
---
+++ t/Fastq.t
@@ -0,0 +1,35 @@
+#! /usr/bin/env perl
+
+use Carp;                 # Caller-relative error messages
+
+use Test::More 'tests' => 1 + 2;   # Main testing module; run this many subtests
+                                   # in BEGIN + subtests (subroutines).
+
+BEGIN {
+	use_ok( 'Bio::SeqWare::Uploads::CgHub::Fastq' );
+}
+
+my $CLASS = 'Bio::SeqWare::Uploads::CgHub::Fastq';
+
+# Create default object for testing
+my $FILE_OBJ = $CLASS->new();
+
+subtest( 'new()' => \&testNew );
+subtest( 'new(BAD)' => \&testNewBAD );
+
+sub testNew {
+    plan( tests => 1 );
+    {
+	    ok($FILE_OBJ, "New object created ok");
+	}
+}
+
+sub testNewBAD {
+	plan( tests => 1 );
+    {
+        eval{ $CLASS->new( "BAD_PARAM"); };
+        $got = $@;
+        $want = qr/^Error string as reg\.exp\./;
+        like( $got, $want, "error with bad param");
+    }
+}
