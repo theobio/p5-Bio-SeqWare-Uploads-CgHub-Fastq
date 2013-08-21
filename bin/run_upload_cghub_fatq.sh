ea29da72b2567d0b5cc07100f253a3f57011b177
Stuart R. Jefferys <srjefferys@gmail.com>
2013-08-21 18:16:52 -0400

    Added run_*.sh file to execute as a cron job.

---
+++ bin/run_upload_cghub_fatq.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# USAGE:
+#    Run the upload-cghub-fastq perl script
+#    $ run_upload-cghub-fastq.sh
+
+VERSION="0.001"
+
+# Enable perlbrew and switch to uncseqperl until done
+export PERLBREW_ROOT="/home/seqware/perl5/perlbrew"
+export PERLBREW_HOME="/tmp/.perlbrew"
+source "${PERLBREW_ROOT}/etc/bashrc"
+perlbrew use uploadperl
+
+COMMAND="upload-cghub-fastq --runMode ZIP"
+
+LOG_HEADING_MESSAGE=" $HOST $DATE"
+
+echo ""
+echo "-----------------------------------------------------------------"
+echo $LOG_HEADING_MESSAGE
+echo "-----------------------------------------------------------------"
+echo "PROGRAM: run_upload-cghub-fastq.sh $VERSION."
+echo "RUNNING: $COMMAND"
+
+$COMMAND
+
+# Not really needed as redirections go away when close top shell, but tidy.
+perlbrew off
